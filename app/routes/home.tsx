99eb43fd7250c8011b379cd58e0b5c9637881d3f
comppy <kim1122aass@gmail.com>
2026-09-22 14:20:09 +0900

    chor:기본 생성 파일 삭제

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "seojoon-blog" },
+    { name: "description", content: "" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main></main>;
 }
